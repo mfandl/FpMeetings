e27f491884c886c2e4d02ff830d868b2ed4e5afe
Matej Fandl <matej@mladypes.sk>
2016-05-09 22:16:34 +0200

    StyleYY

--- src/Meeting1.scala
+++ src/Meeting1.scala
@@ -3,20 +3,19 @@
   */
 object Meeting1 {
   def main(args: Array[String]) {
-    val sampleList = List(1, 2, 3, 4, 5)
-
     def sumList(lst: List[Int]): Int = {
       if (lst.isEmpty) 0
       else lst.head + sumList(lst.tail)
     }
-
-    println(sumList(sampleList))
 
     def myMap(fn: Int => Int, lst: List[Int]): List[Int] = {
       if (lst.isEmpty) List.empty
       else List(fn (lst.head)) ::: myMap(fn, lst.tail)
     }
 
+    val sampleList = List(1, 2, 3, 4, 5)
+
+    println(sumList(sampleList))
     myMap(_ + 1, sampleList) foreach println
   }
 }
